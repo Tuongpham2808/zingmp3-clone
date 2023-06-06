06df5bda00e74a225edee08f676aca8ee933e6ca
TuongGit <tuongphamdh@gmail.com>
2023-06-06 16:34:19 +0700

    #2

--- src/layouts/GlobalLayout.js
+++ src/layouts/GlobalLayout.js
@@ -36,8 +36,8 @@ const GlobalLayout = () => {
         <div
           className={`bgMain blActice slideRight w-[var(--margin-SidebarRight)] overflow-hidden stroke-slate-500 flex-none h-full absolute right-0 top-0 bottom-0 z-[2] ${
             !isOpenSBR
-              ? "translate-x-[100%] opacity-0"
-              : "translate-x-0 opacity-100"
+              ? "!translate-x-[100%] opacity-0"
+              : "!translate-x-0 opacity-100"
           }`}
         >
           <SidebarRight></SidebarRight>
